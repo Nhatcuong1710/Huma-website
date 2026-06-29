00a99ebd6189e271d84fe8c7c9bba492bef27a85
Chris Dao <chris@MacBook-Air-cua-Chris.local>
2026-06-29 14:14:29 +0700

    Fix careers page

---
+++ app/careers/page.tsx
@@ -0,0 +1,8 @@
+export default function CareersPage() {
+  return (
+    <main className="container py-20">
+      <h1 className="text-4xl font-bold">Careers</h1>
+      <p>Welcome to HUMA Careers.</p>
+    </main>
+  );
+}
